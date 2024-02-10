a25a0864d45e332ff504576e4c02babe8d816dce
Faizal Khan <93247599+faizalkhan99@users.noreply.github.com>
2024-02-11 00:22:26 +0530

    New Enemy
    One more type of enemy added + still brainstorming level design.

---
+++ Assets/Scripts/Enemies/SeeAndFollow.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SeeAndFollow : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject _player;
+    CamShake _camShakeHandle;
+    [SerializeField] private int _health;
+    [SerializeField] private int _damageGiven;
+    [SerializeField] private int _damageRecieved;
+    [SerializeField] private int _knockbackForce;
+    [SerializeField] private int _knockBackDuration;
+
+    [SerializeField] private bool _followPlayer = false;
+    private void Awake()
+    {
+        _camShakeHandle = GameObject.Find("Main Camera").GetComponent<CamShake>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(_followPlayer && _player != null)
+        {
+            agent.SetDestination(_player.transform.position);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _followPlayer = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerMovement p = _player.gameObject.GetComponent<PlayerMovement>();
+            p.Damage(_damageGiven, gameObject.transform.forward, _knockbackForce, _knockBackDuration);
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Player_Bullet"))
+        {
+            Destroy(collision.gameObject);
+            Damage();
+        }
+    }
+
+    void Damage()
+    {
+        _health = _health - _damageRecieved;
+        if(_health <= 0)
+        {
+            //play damage sfx here.
+            Die();
+        }
+    }
+    void Die()
+    {
+        _camShakeHandle.EnableShake();
+        Destroy(gameObject);
+    }
+}
